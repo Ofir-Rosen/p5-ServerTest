c78bab707c9d4120bacfe3e61cfd17114b6d07e6
Ofir-Rosen <48698558+Ofir-Rosen@users.noreply.github.com>
2019-11-03 19:43:51 -0500

    Stability fixes in P5 Code

--- sketch.js
+++ sketch.js
@@ -6,49 +6,56 @@ let data = 0;
 let now;
 
 function preload(){
-  url = "http://localHost:8080";
+  url = "http://192.168.0.177/";
   httpGet(url, false, function(response) {
       // when the HTTP request completes, populate the variable that holds the
       // earthquake data used in the visualization.
       data = response;
     });
+    fingers = createVideo('assets/fingers.mov');
+    //fingers = preloadVide
+    //fingers.loop();
+    //button = createButton('play');
+    //button.mousePressed(toggleVid); // attach button listener
+    fingers.hide();
 }
 
 function setup() {
   createCanvas(710, 400);
   now = millis();
-  url = "http://localHost:8080";
+  //IF USING PYTHON
+  //url = "localhost:8080";
+  //IF USING ARDUINO
+  url = "http://192.168.0.177/";
   // specify multiple formats for different browsers
-  fingers = createVideo('assets/fingers.mov');
-  fingers.loop();
-  //button = createButton('play');
-  //button.mousePressed(toggleVid); // attach button listener
-  fingers.hide();
 }
 function draw(){
+  toggleVid();
   if ( millis() > now+1000) {
     getReq();
     console.log(data);
     now = millis();
   }
   if (data == 0){
+    if (playing) playing = false;
     fill(0);
     rect(0,0,width,height);
   } else {
-    fingers.loop();
+    if (!playing) playing = true;
     image(fingers,0,0,width,height);
   }
 
 }// plays or pauses the video depending on current state
 function toggleVid() {
   if (playing) {
-    fingers.loop();
-    button.html('play');
+    fingers.play();
+    //button.html('play');
   } else {
-    fingers.loop();
-    button.html('pause');
+    fingers.stop();
+    //button.html('pause');
   }
-  playing = !playing;
+    lp = playing;
+  //playing = !playing;
 }
 
 function getReq(){
